bb9b12ca31450b30b6767d299b0399b1a2d94db5
icovv <hvasilev00@abv.bg>
2025-02-01 17:40:08 +0200

    setting up controllers for the different routes

--- src/config/configRoutes.js
+++ src/config/configRoutes.js
@@ -1,8 +1,12 @@
+const {dataRouter} = require('../controllers/adminData');
+const {adminRouter} = require('../controllers/data');
+const {userRouter} = require('../controllers/user');
 
 
 
 function configRoutes(app){
-    
+    app.use('/data', dataRouter);
+    app.use('/admin', adminRouter);
 }
 
 module.exports = {configRoutes};
---
+++ src/controllers/adminData.js
@@ -0,0 +1,6 @@
+const {Router} = require('express')
+
+const adminRouter = Router();
+
+
+module.exports = {adminRouter};
