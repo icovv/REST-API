1b7b8925570dde021652b54125d03ea3ab93a07e
icovv <hvasilev00@abv.bg>
2025-02-02 21:56:05 +0200

    creating delete and update mechanism for the bedroom items.

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -33,7 +33,7 @@ dataRouter.get('/bedroom/:id', async(req,res) => {
     try {
         const image = await Bedroom.findById(id);
         if(!image){
-            return res.status(400).send("image not found");
+            return res.status(400).send("Bedroom item not found!");
         }
         res.send({
             image: image.picture.toString('base64'),
@@ -43,8 +43,8 @@ dataRouter.get('/bedroom/:id', async(req,res) => {
             characteristics: image.characteristics,
             contentType: image.contentType});
     } catch (error) {
-        console.error('Error fetching image:', error);
-        res.status(500).send({ code: 500, message: 'Error fetching image'});
+        console.error('Error fetching bedroom item:', error);
+        res.status(500).send({ code: 500, message: 'Error fetching bedroom item!'});
     }
 })
 dataRouter.post('/admin/bedroom',isUser(),isAdmin(),upload.single('image'), async(req,res) => {
@@ -62,22 +62,51 @@ dataRouter.post('/admin/bedroom',isUser(),isAdmin(),upload.single('image'), asyn
 
     try {
         await bedroomWithImg.save();
-        res.status(200).json({ code: 200, message: 'Image uploaded successfully', imageId: bedroomWithImg._id });
+        res.status(200).json({ code: 200, message: 'Bedroom item uploaded successfully!', imageId: bedroomWithImg._id });
       } catch (error) {
         console.error('Error uploading image:', error);
-        res.status(500).send({ code: 500, message: 'Error uploading image'});
+        res.status(500).send({ code: 500, message: 'Error uploading bedroom item!'});
       }
 
 })
 
-dataRouter.delete('/admin/bedroom/:id', async(req,res) => {
+dataRouter.delete('/admin/bedroom/:id',isUser(),isAdmin(), async(req,res) => {
     let {id} = req.params;
 
     try {
-        
+        const result = await Bedroom.findByIdAndDelete(id);
+        if(!result) {
+            res.status(404).json({code:404, message:"Bedroom item not found!"})
+        }
+        res.status(200).json({code:200, message: 'Bedroom item deleted successfully!' });
     } catch (error) {
-        
+      console.error('Error deleting image:', error);
+      res.status(500).json({code:500, message: 'Error deleting bedroom item!' });
     }
+})
+dataRouter.put('/admin/bedroom/:id',isUser(),isAdmin(),upload.single('image'), async(req,res) => {
+    let {id} = req.params;
+    const {tittle,price,description,characteristics} = req.body;
+    const {originalName, buffer, mimetype} = req.file;
+
+    try {
+        let image = await Bedroom.findById(id);
+        if(!image){
+            res.status(404).json({code:404, message:"Bedroom item not found!"})
+        }
+        image.tittle = tittle;
+        image.price = price;
+        image.description = description;
+        image.characteristics = characteristics;
+        image.picture = buffer;
+        image.contentType = mimetype;
+
+        res.status(200).json({ code: 200, message: 'Bedroom item changed successfully', imageId: image._id });
+      } catch (error) {
+        console.error('Error changing image:', error);
+        res.status(500).send({ code: 500, message: 'Error changing Bedroom item'});
+      }
+
 })
 
 module.exports = {dataRouter}
