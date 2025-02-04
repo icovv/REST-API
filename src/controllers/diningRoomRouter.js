ddace4581792c19bae5ebedf3288f37b789dbc42
icovv <hvasilev00@abv.bg>
2025-02-04 23:00:21 +0200

    create dining room item (for admin)

--- src/controllers/diningRoomRouter.js
+++ src/controllers/diningRoomRouter.js
@@ -50,3 +50,41 @@ bedroomRouter.get('/dining-room/:id', async(req,res) => {
         res.status(500).json({ code: 500, message: ['Error fetching Dining room item!']});
     }
 })
+
+bedroomRouter.post('/admin/dining-room',
+    isUser(),
+    isAdmin(),
+    upload.single('image'),
+    fileFilter(),
+    body('tittle').trim().isString().withMessage('Please enter valid tittle!').notEmpty().withMessage('Please enter valid tittle!'),
+    body('characteristics').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('description').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('price').trim().isNumeric().withMessage("Please enter valid price!").notEmpty().withMessage("Please enter valid price!"),
+    body('col').trim().isString().withMessage("Please enter valid collection name!").notEmpty().withMessage("Please enter valid price!"),
+    async(req,res) => {
+    const {tittle,price,description,characteristics,col} = req.body;
+    const {originalName, buffer, mimetype} = req.file;
+
+    let item = new DiningRoom({
+        tittle,
+        col,
+        price,
+        description,
+        characteristics,
+        picture: buffer,
+        contentType: mimetype
+    })
+
+    try {
+        const isResultValid = validationResult(req);
+        if (isResultValid.errors.length){
+            let parsedErr = parseError(error)
+            res.status(500).json({ code: 500, message: Object.values(parsedErr.errors)});
+        }
+        await item.save();
+        res.status(200).json({ code: 200, message: ['Dining room item uploaded successfully!'], itemId: item._id });
+      } catch (error) {
+        res.status(500).json({ code: 500, message: ["An error occured while loading your item!"]});
+      }
+
+})
