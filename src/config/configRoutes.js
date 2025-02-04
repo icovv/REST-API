145ef3ba7361d09c5d904b7f8c5f4d64a0edeff0
icovv <hvasilev00@abv.bg>
2025-02-04 23:07:53 +0200

    implementing full decorRouter functionality (CRUD) the same as the other routers and adding it to the configRoutes.

--- src/config/configRoutes.js
+++ src/config/configRoutes.js
@@ -1,6 +1,7 @@
 const {userRouter} = require('../controllers/userRouter');
 const {bedroomRouter} = require('../controllers/bedroomRouter');
 const { diningRoomRouter } = require('../controllers/diningRoomRouter');
+const { decorRouter } = require('../controllers/decorRouter');
 
 
 
@@ -8,6 +9,7 @@ function configRoutes(app){
     app.use(bedroomRouter);
     app.use(userRouter)
     app.use(diningRoomRouter)
+    app.use(decorRouter)
 }
 
 module.exports = {configRoutes};
---
+++ src/controllers/decorRouter.js
@@ -0,0 +1,159 @@
+const {Router} = require('express')
+const {isAdmin, isUser} = require('../midlewares/guards')
+const multer = require('multer')
+const {Decor} = require('../models/decor')
+const {body,validationResult} = require('express-validator');
+const {fileFilter} = require('../midlewares/fileFilter')
+const {parseError} = require('../utils/errorParser')
+
+const decorRouter = Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({storage:storage});
+
+
+
+decorRouter.get('/decor', async(req,res) => {
+    try {
+        const items = await Decor.find().lean();
+        console.log(items, "tuk");
+        if(items.length < 1){
+            return res.status(400).json({code: 400, message:["No Decor items were found!"]});
+        }
+        if(items.length > 0){
+        items.forEach((el) => el.picture.toString("base64"))
+        }
+        res.status(200).json({code: 200, items: items});
+    } catch (error) {
+        res.status(500).json({ code: 500, message: ['Error fetching Decor items!']});
+    }
+})
+
+decorRouter.get('/decor/:id', async(req,res) => {
+    const {id } = req.params;
+
+    try {
+        const item = await Decor.findById(id).lean();
+        if(!item){
+            return res.status(400).json({code: 400, message:["Decor item not found!"]});
+        }
+        res.status(200).res.json({
+            itemId: item._id,
+            picture: item.picture.toString('base64'),
+            col: item.col,
+            tittle: item.tittle,
+            price: item.price,
+            description: item.description,
+            characteristics: item.characteristics,
+            contentType: item.contentType});
+    } catch (error) {
+        res.status(500).json({ code: 500, message: ['Error fetching Decor item!']});
+    }
+})
+decorRouter.post('/admin/decor',
+    isUser(),
+    isAdmin(),
+    upload.single('image'),
+    fileFilter(),
+    body('tittle').trim().isString().withMessage('Please enter valid tittle!').notEmpty().withMessage('Please enter valid tittle!'),
+    body('characteristics').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('description').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('price').trim().isNumeric().withMessage("Please enter valid price!").notEmpty().withMessage("Please enter valid price!"),
+    body('col').trim().isString().withMessage("Please enter valid collection name!").notEmpty().withMessage("Please enter valid price!"),
+    async(req,res) => {
+    const {tittle,price,description,characteristics,col} = req.body;
+    const {originalName, buffer, mimetype} = req.file;
+
+    let item = new Decor({
+        tittle,
+        col,
+        price,
+        description,
+        characteristics,
+        picture: buffer,
+        contentType: mimetype
+    })
+
+    try {
+        const isResultValid = validationResult(req);
+        if (isResultValid.errors.length){
+            let parsedErr = parseError(error)
+            res.status(500).json({ code: 500, message: Object.values(parsedErr.errors)});
+        }
+        await item.save();
+        res.status(200).json({ code: 200, message: ['Decor item uploaded successfully!'], itemId: item._id });
+      } catch (error) {
+        res.status(500).json({ code: 500, message: ["An error occured while loading your item!"]});
+      }
+
+})
+
+decorRouter.delete('/admin/decor/:id',isUser(),isAdmin(), async(req,res) => {
+    let {id} = req.params;
+
+    try {
+        const item = await Decor.findByIdAndDelete(id).lean();
+        if(!item) {
+            res.status(404).json({code:404, message:["Decor item not found!"]})
+        }
+        res.status(200).json({code:200, message: ['Decor item deleted successfully!'] });
+    } catch (error) {
+      res.status(500).json({code:500, message: ['Error deleting Decor item!'] });
+    }
+})
+decorRouter.put('/admin/decor/:id',
+    isUser(),
+    isAdmin(),
+    upload.single('image'),
+    fileFilter(),
+    body('tittle').trim().isString().withMessage('Please enter valid tittle!').notEmpty().withMessage('Please enter valid tittle!'),
+    body('characteristics').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('description').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('price').trim().isNumeric().withMessage("Please enter valid price!").notEmpty().withMessage("Please enter valid price!"),
+    body('col').trim().isString().withMessage("Please enter valid collection name!").notEmpty().withMessage("Please enter valid price!"),
+    async(req,res) => {
+    let {id} = req.params;
+    const {tittle,price,description,characteristics,col} = req.body;
+    const {originalName, buffer, mimetype} = req.file;
+
+    try {
+        const isResultValid = validationResult(req);
+        if (isResultValid.errors.length){
+            let parsedErr = parseError(error)
+            res.status(500).json({ code: 500, message: Object.values(parsedErr.errors)});
+        }
+        let item = await Decor.findByIdAndUpdate(
+            id,
+            {
+                tittle:tittle,
+                col:col,
+                price: price,
+                description: description,
+                characteristics: characteristics,
+                picture: buffer,
+                contentType: mimetype
+            },
+            {new: true}
+        ).lean();
+        if(!item){
+            res.status(404).json({code:404, message:["Decor item not found!"]})
+        }
+
+        res.status(200).json({
+            code: 200,
+            message: ['Decor item changed successfully'],
+            itemId: item._id,
+            col: item.col,
+            picture: item.picture.toString('base64'),
+            tittle: item.tittle,
+            price: item.price,
+            description: item.description,
+            characteristics: item.characteristics,
+            contentType: item.contentType});
+      } catch (error) {
+        res.status(500).json({ code: 500, message: ["An error occured while loading your item!"]});
+      }
+
+})
+
+module.exports = {decorRouter}
