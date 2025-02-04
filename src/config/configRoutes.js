8b1c83ce89409fd57c069f661a0d737c6fccc48f
icovv <hvasilev00@abv.bg>
2025-02-04 23:03:24 +0200

    delete and update for dining room items and also importing the router into configRoutes

--- src/config/configRoutes.js
+++ src/config/configRoutes.js
@@ -1,11 +1,13 @@
 const {userRouter} = require('../controllers/userRouter');
-const {bedroomRouter} = require('../controllers/bedroomRouter')
+const {bedroomRouter} = require('../controllers/bedroomRouter');
+const { diningRoomRouter } = require('../controllers/diningRoomRouter');
 
 
 
 function configRoutes(app){
     app.use(bedroomRouter);
     app.use(userRouter)
+    app.use(diningRoomRouter)
 }
 
 module.exports = {configRoutes};
--- src/controllers/diningRoomRouter.js
+++ src/controllers/diningRoomRouter.js
@@ -88,3 +88,73 @@ bedroomRouter.post('/admin/dining-room',
       }
 
 })
+
+bedroomRouter.delete('/admin/dining-room/:id',isUser(),isAdmin(), async(req,res) => {
+    let {id} = req.params;
+
+    try {
+        const item = await DiningRoom.findByIdAndDelete(id).lean();
+        if(!item) {
+            res.status(404).json({code:404, message:["Dining room item not found!"]})
+        }
+        res.status(200).json({code:200, message: ['Dining room item deleted successfully!'] });
+    } catch (error) {
+      res.status(500).json({code:500, message: ['Error deleting Dining room item!'] });
+    }
+})
+bedroomRouter.put('/admin/dining-room/:id',
+    isUser(),
+    isAdmin(),
+    upload.single('image'),
+    fileFilter(),
+    body('tittle').trim().isString().withMessage('Please enter valid tittle!').notEmpty().withMessage('Please enter valid tittle!'),
+    body('characteristics').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('description').trim().isString().withMessage('Please enter valid characteristics!').notEmpty().withMessage('Please enter valid characteristics!'),
+    body('price').trim().isNumeric().withMessage("Please enter valid price!").notEmpty().withMessage("Please enter valid price!"),
+    body('col').trim().isString().withMessage("Please enter valid collection name!").notEmpty().withMessage("Please enter valid price!"),
+    async(req,res) => {
+    let {id} = req.params;
+    const {tittle,price,description,characteristics,col} = req.body;
+    const {originalName, buffer, mimetype} = req.file;
+
+    try {
+        const isResultValid = validationResult(req);
+        if (isResultValid.errors.length){
+            let parsedErr = parseError(error)
+            res.status(500).json({ code: 500, message: Object.values(parsedErr.errors)});
+        }
+        let item = await DiningRoom.findByIdAndUpdate(
+            id,
+            {
+                tittle:tittle,
+                col:col,
+                price: price,
+                description: description,
+                characteristics: characteristics,
+                picture: buffer,
+                contentType: mimetype
+            },
+            {new: true}
+        ).lean();
+        if(!item){
+            res.status(404).json({code:404, message:["Dining room item not found!"]})
+        }
+
+        res.status(200).json({
+            code: 200,
+            message: ['Dining room changed successfully'],
+            itemId: item._id,
+            col: item.col,
+            picture: item.picture.toString('base64'),
+            tittle: item.tittle,
+            price: item.price,
+            description: item.description,
+            characteristics: item.characteristics,
+            contentType: item.contentType});
+      } catch (error) {
+        res.status(500).json({ code: 500, message: ["An error occured while loading your item!"]});
+      }
+
+})
+
+module.exports = {diningRoomRouter}
