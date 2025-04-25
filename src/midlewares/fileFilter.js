18b12b88353a0fbb3d4acc40ee6584dc214e12ca
icovv <hvasilev00@abv.bg>
2025-04-25 18:15:57 +0300

    changes to error handleing in the fileFilter

--- src/midlewares/fileFilter.js
+++ src/midlewares/fileFilter.js
@@ -2,11 +2,13 @@ function fileFilter(){
     return function (req,res,next){
     let fileTypes = ["image/jpeg", "image/png", "image/gif", "image/jpg"]
     if(!req.file){
-        res.status(500).send({ code: 500, message: "Please make sure you are uploading a picture!"});  
+        res.status(500).send({ code: 500, message: ["Please make sure you are uploading a picture!"]});
+        return; 
     }
 
     if(!fileTypes.includes(req.file.mimetype)) {
-        res.status(500).send({ code: 500, message: "Please make sure you are uploading a picture!"});
+        res.status(500).send({ code: 500, message: ["Please make sure you are uploading a picture!"]});
+        return;
     }
     next()
     }
