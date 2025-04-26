a538172762808a6ed39ee6f28d2c56c9e0195720
icovv <hvasilev00@abv.bg>
2025-04-26 15:34:08 +0300

    improving fileFilter middleware to handle requests without new image.

--- src/midlewares/fileFilter.js
+++ src/midlewares/fileFilter.js
@@ -1,6 +1,12 @@
 function fileFilter(){
     return function (req,res,next){
     let fileTypes = ["image/jpeg", "image/png", "image/gif", "image/jpg"]
+    console.log(req.route.path);
+    console.log(req.method);
+    if(!req.file && req.method == "PUT"){
+        next();
+        return;
+    }
     if(!req.file){
         res.status(500).send({ code: 500, message: ["Please make sure you are uploading a picture!"]});
         return; 
